07b8304f4be12cdfb9c3465b292bd9d76fbe4261
pikampg <116376602+pikampg@users.noreply.github.com>
2023-02-03 15:40:24 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,3 +1,17 @@
+"""
+Удалить информацию хранящуюся в ключе PatientName (анонимизировать файлы)
+
+Используя информацию в ключах StudyInstanceUID, SeriesInstanceUID, SOPInstanceUID преобразовать структуру хранения файлов к следующей:
+
+    - на первом уровне StudyInstanceUID
+    - на втором уровне SeriesInstanceUID
+    - именем файла будет значение SOPInstanceUID с расширением .dcm
+    
+Таким образом, путь к каждому файлу будет выглядеть так: $StudyInstanceUID/$SeriesInstanceUID/$SOPInstanceUID.dcm
+
+Дополнительно, нужно создать файл, в котором путь к каждому файлу исходной структуры сопоставлен пути к файлу в конечной структуре.
+"""
+
 from pathlib import Path
 import json
 import pydicom
@@ -8,7 +22,7 @@ PATH_TO_PATIENTS = 'src'
 def prepare_folders(file_dir: str) -> None:
     folder = Path(file_dir)
     file_map = {}
-    c = 0
+    
     for file in folder.glob('*'):
         dataset = pydicom.read_file(file)
 
@@ -24,9 +38,7 @@ def prepare_folders(file_dir: str) -> None:
         dataset.save_as(end_file_path)
 
         file_map[str(file)] = end_file_path
-        c += 1
-        if c == 5:
-            break
+ 
 
     with open("mapping.json", "a") as f:
         f.write(json.dumps(file_map, indent=4))
